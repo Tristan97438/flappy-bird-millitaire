4b78af32ea603ec9579b3636341d5feb6ecd2e62
Tristan <t.laurent@rubika-edu.re>
2025-11-25 00:38:06 +0400

    ajout 3 eme mur + tank

--- Assets/scripts/spawn mur.cs
+++ Assets/scripts/spawn mur.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 public class spawnmur : MonoBehaviour
 {
-    [SerializeField] GameObject mur1, mur2;
+    [SerializeField] GameObject mur1, mur2,mur3;
     [SerializeField] GameObject canva_game_over;
+    float temp_entre_mur;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        temp_entre_mur = 2f;
         StartCoroutine(spawn());
     }
 
@@ -23,18 +25,28 @@ public class spawnmur : MonoBehaviour
             if(!canva_game_over.activeSelf && GameObject.FindWithTag("bird").GetComponent<mortbird>().mort_b == false)
             {
                 Instantiate(mur1, gameObject.transform.position, Quaternion.identity);
-                yield return new WaitForSeconds(2);
+                yield return new WaitForSeconds(temp_entre_mur);
                 
             }
             if (!canva_game_over.activeSelf && GameObject.FindWithTag("bird").GetComponent<mortbird>().mort_b == false)
             {
                 
                 Instantiate(mur2, gameObject.transform.position, Quaternion.identity);
-                yield return new WaitForSeconds(2);
+                yield return new WaitForSeconds(temp_entre_mur);
+            }
+            if (!canva_game_over.activeSelf && GameObject.FindWithTag("bird").GetComponent<mortbird>().mort_b == false)
+            {
+
+                Instantiate(mur3, gameObject.transform.position, Quaternion.identity);
+                yield return new WaitForSeconds(temp_entre_mur);
             }
             else
             {
                 yield return new WaitForSeconds(0.1f);
+            }
+            if(GetComponent<score>().score_joueur%5 == 0)
+            {
+                temp_entre_mur -= 0.1f;
             }
             
         }
